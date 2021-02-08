8aadf64b4154f6971f1c731a8cb4246fa51e6945
bilal-fazlani <bilal.m.fazlani@gmail.com>
2021-02-08 19:28:35 +0530

    restructure build.sbt

--- project/Libs.scala
+++ project/Libs.scala
@@ -15,19 +15,25 @@ object Libs {
 
 
   lazy val `slf4j-simple` = "org.slf4j" % "slf4j-simple" % "2.0.0-alpha1"
+  
   lazy val `akka-http` = ("com.typesafe.akka" %% "akka-http" % AkkaHttpVersion)
     .cross(CrossVersion.For3Use2_13())
-  lazy val `akka-actor-typed` =
-    ("com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion).cross(
-      CrossVersion.For3Use2_13()
-    )
+  
+    lazy val `akka-actor-typed` =
+    ("com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion)
+    .cross(CrossVersion.For3Use2_13())
+
   lazy val `akka-testkit` =
-    ("com.typesafe.akka" %% "akka-actor-testkit-typed" % AkkaVersion).cross(
-      CrossVersion.For3Use2_13()
-    )
+    ("com.typesafe.akka" %% "akka-actor-testkit-typed" % AkkaVersion)
+    .cross(CrossVersion.For3Use2_13())
+
+  lazy val `akka-http-testkit` = ("com.typesafe.akka" %% "akka-http-testkit" % AkkaHttpVersion)
+    .cross(CrossVersion.For3Use2_13())
+
   lazy val `akka-stream` = ("com.typesafe.akka" %% "akka-stream" % AkkaVersion)
     .cross(CrossVersion.For3Use2_13())
-  lazy val `jwt-core` =
+  
+    lazy val `jwt-core` =
     ("com.pauldijou" %% "jwt-core" % "5.0.0").cross(CrossVersion.For3Use2_13())
 }
 
